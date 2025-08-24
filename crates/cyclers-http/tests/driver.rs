84945b2316f6e8b5a57bb77f4b23c6e5bf639b37
Harvey <324418+leesiongchan@users.noreply.github.com>
2025-08-24 21:52:49 +0800

    Add concurrent request test for HttpDriver

--- crates/cyclers-http/tests/driver.rs
+++ crates/cyclers-http/tests/driver.rs
@@ -1,6 +1,9 @@
+use std::time::Duration;
+
 use anyhow::Result;
 use cyclers::driver::Driver as _;
 use cyclers_http::{ClientBuilder, HttpCommand, HttpDriver, HttpSource};
+use futures_concurrency::future::TryJoin as _;
 use futures_lite::{StreamExt as _, pin, stream};
 #[cfg(not(target_family = "wasm"))]
 use tokio::test;
@@ -53,15 +56,68 @@ async fn it_receives_responses() -> Result<()> {
         .map(|res| res.map_err(anyhow::Error::from_boxed));
     pin!(response);
 
-    fut.await.map_err(anyhow::Error::from_boxed)?;
-
-    {
-        let response = response.next().await.unwrap()?;
+    (fut, async move {
+        let response = response.try_next().await?.unwrap();
         assert_eq!(response.body(), "world".as_bytes());
         api_mock.assert();
-    }
+        Ok(())
+    })
+        .try_join()
+        .await
+        .map(|_| ())
+        .map_err(anyhow::Error::from_boxed)
+}
 
-    assert!(response.next().await.is_none());
+#[test]
+async fn it_handles_concurrent_requests() -> Result<()> {
+    let server = httpmock::MockServer::start_async().await;
+    let hello_mock = server
+        .mock_async(|when, then| {
+            when.path("/hello");
+            then
+                .status(200)
+                .body("world")
+                // Simulates a network delay
+                .delay(Duration::from_secs(1));
+        })
+        .await;
+    let foobar_mock = server
+        .mock_async(|when, then| {
+            when.path("/foo");
+            then.status(200).body("bar");
+        })
+        .await;
 
-    Ok(())
+    let sink = stream::iter([
+        HttpCommand::from(
+            http::Request::builder()
+                .method("GET")
+                .uri(server.url("/hello"))
+                .body(vec![].into())?,
+        ),
+        HttpCommand::from(
+            http::Request::builder()
+                .method("GET")
+                .uri(server.url("/foo"))
+                .body(vec![].into())?,
+        ),
+    ]);
+    let (source, fut): (HttpSource<_>, _) = HttpDriver.call(sink);
+
+    let response = source
+        .responses()
+        .map(|res| res.map_err(anyhow::Error::from_boxed));
+    pin!(response);
+
+    (fut, async {
+        let response = response.try_next().await?.unwrap();
+        assert_eq!(response.body(), "bar".as_bytes());
+        hello_mock.assert();
+        foobar_mock.assert();
+        Ok(())
+    })
+        .try_join()
+        .await
+        .map(|_| ())
+        .map_err(anyhow::Error::from_boxed)
 }
